eb6a9e274fe4e96d3430d84c9845adbde0610750
Grekkq <grekkq97@gmail.com>
2022-05-25 13:11:23 +0200

    Handling docker secrets and diffrent approach to logging

--- bpw.go
+++ bpw.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/1Password/connect-sdk-go/connect"
+	secrets "github.com/ijustfool/docker-secrets"
 )
 
 type Entry struct {
@@ -28,37 +29,63 @@ func parseIntFromUrl(parameter_name string, r *http.Request) (int, error) {
 }
 
 func addEntry(w http.ResponseWriter, r *http.Request) {
+	log.Print("Received http request.")
 	sys_parameter_name, dia_parameter_name, pulse_parameter_name := "sys", "dia", "pulse"
 	parsedSys, err := parseIntFromUrl(sys_parameter_name, r)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Please provide valid %v. %v", sys_parameter_name, err.Error()), http.StatusInternalServerError)
-		return
+		log.Print(err)
+		panic(fmt.Sprintf("Please provide valid value for %v.", sys_parameter_name))
 	}
 	parsedDia, err := parseIntFromUrl(dia_parameter_name, r)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Please provide valid %v. %v", dia_parameter_name, err.Error()), http.StatusInternalServerError)
-		return
+		log.Print(err)
+		panic(fmt.Sprintf("Please provide valid value for %v.", dia_parameter_name))
 	}
 	parsedPulse, err := parseIntFromUrl(pulse_parameter_name, r)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Please provide valid %v. %v", pulse_parameter_name, err.Error()), http.StatusInternalServerError)
-		return
+		log.Print(err)
+		panic(fmt.Sprintf("Please provide valid value for %v.", pulse_parameter_name))
 	}
 	entry := Entry{parsedSys, parsedDia, parsedPulse, int(time.Now().Unix())}
+
+	dockerSecrets, _ := secrets.NewDockerSecrets("")
+	address, _ := dockerSecrets.Get("1password_http_address")
+	token, _ := dockerSecrets.Get("1password_api_token")
+	vaultName, _ := dockerSecrets.Get("1password_vault_name")
+	dbConnectionStringName, _ := dockerSecrets.Get("db_connection_string_name")
+
+	client := connect.NewClient(address, token)
+	item, err := client.GetItemByTitle(dbConnectionStringName, vaultName)
+	if err != nil {
+		log.Print(err)
+		panic("Failed to connect to database.")
+	}
+
+	for i := range item.Fields {
+		if item.Fields[i].Label == "password" {
+			log.Print(item.Fields[i].Value)
+			break
+		}
+	}
+
 	fmt.Fprintf(w, "Entry added test: %v", entry)
-	log.Print("Received http request.")
+}
+
+func handleAddEntry(w http.ResponseWriter, r *http.Request) {
+	defer handlePanic(w)
+	addEntry(w, r)
+}
+
+func handlePanic(w http.ResponseWriter) {
+	if r := recover(); r != nil {
+		http.Error(w, fmt.Sprintf("%v\nServer encounter an error, please try again later.", r), http.StatusInternalServerError)
+	}
 }
 
 func main() {
 	t := time.Now()
 	fmt.Println(t.Unix())
-	client := connect.NewClient("<your_connect_host>", "<your_connect_token>")
-	item, err := client.GetItem("<item-uuid>", "<vault-uuid>")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Print(item)
 
-	http.HandleFunc("/addEntry/", addEntry)
+	http.HandleFunc("/addEntry/", handleAddEntry)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module bpw
 
 go 1.18
 
-require github.com/1Password/connect-sdk-go v1.4.0
+require (
+	github.com/1Password/connect-sdk-go v1.4.0
+	github.com/ijustfool/docker-secrets v0.0.0-20191021062307-b25ea5007562
+)
 
 require (
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/uber/jaeger-client-go v2.30.0+incompatible // indirect
